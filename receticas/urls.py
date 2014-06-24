4b65304d64d747df8ac2f5dba5e351f18523af27
silver <silver@silver-VirtualBox.(none)>
2014-06-23 23:19:38 -0600

    receticas

--- receticas/urls.py
+++ receticas/urls.py
@@ -13,14 +13,14 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'receticas.views.home', name='home'),
+    url(r'^$', 'receticas.views.index'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/rece/'}),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/$', 'auth.views.login_user'),
     url(r'^home/$', 'receticas.views.home'),
     url(r'^accounts/', include('allauth.urls')),
-    url(r'^rece/', 'receticas.views.index'),
+    #url(r'^rece/', 'receticas.views.index'),
     #################Recetas
     url(r'^recetas/(?P<pk>\d+)/$', RecetaDisplayView.as_view(
 	context_object_name="receta",
