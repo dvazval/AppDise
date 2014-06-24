60da533076ba43bbfabbfbdb8a5b7cae3f3ae870
silver <silver@silver-VirtualBox.(none)>
2014-06-24 17:34:58 -0600

    receticas

--- receticas/forms.py
+++ receticas/forms.py
@@ -102,7 +102,7 @@ class CreateIngrediente(ModelForm):
 	
     def save(self, commit=True, force_insert=False, force_update=False, *args, **kwargs):
         m = super(CreateIngrediente, self).save(commit=False, *args, **kwargs)
-	m.idingrediente_id = Ingrediente.objects.get(pk=self.fields['idingredienteTemp'].get(value, u''))   #fields['idingredienteTemp'].choices )   
+	m.idingrediente_id = Ingrediente.objects.get(pk=self.fields['idingredienteTemp'])   #fields['idingredienteTemp'].choices )   
 	m.idreceta_id = self.receta
 	m.save()
 	
